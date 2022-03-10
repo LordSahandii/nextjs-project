49a31bb7568acb91e6ac5dda8fa8da20324179df
Lordsahandii <Sahand2k24@gmail.com>
2022-03-09 23:35:34 -0800

    hmm

--- project1/pages/_app.js
+++ project1/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import {ChakraProvider} from "@chakra-ui/react"
 import { extendTheme } from '@chakra-ui/react'
+import Head from 'next/head'
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
@@ -16,6 +17,11 @@ const theme = extendTheme({ colors })
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Travelling app</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
