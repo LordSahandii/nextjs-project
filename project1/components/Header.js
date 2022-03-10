98cdf5294a9d8dc794b37920ceb844c2a7561e34
LordSahandii <66194412+LordSahandii@users.noreply.github.com>
2022-03-10 11:24:23 -0800

    added header

---
+++ project1/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Center, Container, Heading } from '@chakra-ui/react'
+import Head from 'next/head'
+
+const Header = () => {
+  return (
+        <Container minH="full" minWidth="100%" W="full" position="fixed" p={0} >
+            <Center bg="teal.400" minWidth="full" rounded="md" fontFamily="mono" >
+                Travelgram
+            </Center>
+        </Container>
+
+
+  )
+}
+
+export default Header
--- project1/pages/index.js
+++ project1/pages/index.js
@@ -3,18 +3,25 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { color, Heading, theme, Flex, VStack } from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <Container maxW="container.xl" p={0}>
-      
+    <>
+    
+    <Container maxW="container.xxl" p={0}>
+      <Header/>
+    
+      {/* <Header/> */}
       <Flex h="100vh" py={20}>
-        <VStack w="full" h="full" p={10}>
+        
+        {/* <VStack w="full" h="full" p={10}>
 
-        </VStack>
+        </VStack> */}
 
       </Flex>
 
       </Container>
+      </>
   )
 }
