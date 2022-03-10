596de5c44a462831d7861ac39894ae384a73538c
LordSahandii <66194412+LordSahandii@users.noreply.github.com>
2022-03-10 14:34:04 -0800

    fixed the icons

--- project1/components/Menuu.js
+++ project1/components/Menuu.js
@@ -1,6 +1,9 @@
 import { Container,Flex, VStack, Menu, MenuButton, Button, MenuList, MenuGroup, MenuItem, MenuDivider, HStack, Box, Spacer, useColorMode } from '@chakra-ui/react'
 import React, { useState } from 'react'
-import { IconButton,HamburgerIcon,AddIcon,ExternalLinkIcon,RepeatIcon, EditIcon  } from '@chakra-ui/react'
+import { AddIcon,ExternalLinkIcon,RepeatIcon, EditIcon  } from '@chakra-ui/icons'
+import { Icon } from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { IconButton } from '@chakra-ui/react'
 
 const Menuu = () => {
   const [theeme, setTheme] = useState('light');
@@ -10,53 +13,55 @@ const Menuu = () => {
     setTheme(theeme=='light' ? 'dark' : 'light');
   }
   
+  
   return (
     <Box p={7} h={0} w="full" position="fixed">
-  <Flex >
+  <Flex>
     <Menu>
       <MenuButton
         as={IconButton}
         aria-label='Options'
-        icon={HamburgerIcon}
+        icon={<HamburgerIcon></HamburgerIcon>}
         variant='outline'
       />
       <MenuList>
-        <MenuItem icon={AddIcon } command='⌘T'>
+        <MenuItem icon={<AddIcon/> } command='⌘T'>
           New Tab
         </MenuItem>
-        <MenuItem icon={ExternalLinkIcon } command='⌘N'>
+        <MenuItem icon={<ExternalLinkIcon></ExternalLinkIcon>} command='⌘N'>
           New Window
         </MenuItem>
-        <MenuItem icon={RepeatIcon } command='⌘⇧N'>
+        <MenuItem icon={<RepeatIcon/> } command='⌘⇧N'>
           Open Closed Tab
         </MenuItem>
-        <MenuItem icon={EditIcon } command='⌘O'>
+        <MenuItem icon={<EditIcon></EditIcon> } command='⌘O'>
           Open File...
         </MenuItem>
       </MenuList>
     </Menu>
   
     <Spacer />
+    <HStack spacing='10px'>
+    <Button rounded={10} onClick={(e)=>{ toggleColorMode(); s(e)}} paddingRight={5}>{theeme}</Button>
     
+    <Menu>
+      <MenuButton as={Button} colorScheme='pink'>
+        Profile
+      </MenuButton>
+      <MenuList>
+        <MenuGroup title='Profile'>
+          <MenuItem>My Account</MenuItem>
+          <MenuItem>Payments </MenuItem>
+        </MenuGroup>
+        <MenuDivider />
+        <MenuGroup title='Help'>
+          <MenuItem>Docs</MenuItem>
+          <MenuItem>FAQ</MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
+  </HStack>
     
-    <Spacer />
-    <Button rounded={10} onClick={(e)=>{ toggleColorMode(); s(e)}}>{theeme}</Button>
-      <Menu>
-        <MenuButton as={Button} colorScheme='pink'>
-          Profile
-        </MenuButton>
-        <MenuList>
-          <MenuGroup title='Profile'>
-            <MenuItem>My Account</MenuItem>
-            <MenuItem>Payments </MenuItem>
-          </MenuGroup>
-          <MenuDivider />
-          <MenuGroup title='Help'>
-            <MenuItem>Docs</MenuItem>
-            <MenuItem>FAQ</MenuItem>
-          </MenuGroup>
-        </MenuList>
-      </Menu>
     
   </Flex>
   </Box>
